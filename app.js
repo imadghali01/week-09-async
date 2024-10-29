a527a11745d2fbbabab5550eff3dc35a4fe97355
imadghali01 <imadghali01@gmail.com>
2024-10-29 11:02:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,24 @@
-/*
-    Write a program that gives the body background the color blue
-    1 second later it becomes yellow
-    1 second later it becomes red
-    1 second later it becomes cyan
-    1 second later it becomes violet
-    1 second later it becomes green
-    1 second later it becomes purple
-
-Sounds pretty simple isn'it ?
-
-But here are the restrictions :
-
-    You CAN'T use setInterval
-    You MUST set your setTimeout to 1 second for each color. (no doing maths counting 1,2,3,4,5 etc.)
-*/
 const body = document.querySelector('body');
 const colorArray = ["yellow", "red", "cyan", "violet", "green", "purple"];
 
 body.style.height = '100vh';
-body.style.backgroundColor = `${colorArray[0]}`
-function changeBGColor(array){
-    setTimeout(() => {
-        body.style.backgroundColor =`${array[1]}`;
+
+// Fonction qui retourne une promesse pour changer la couleur après 1 seconde
+function changeBGColor(color) {
+    return new Promise((resolve) => {
         setTimeout(() => {
-            body.style.backgroundColor =`${array[2]}`;
-            setTimeout(() => {
-                body.style.backgroundColor =`${array[3]}`;
-                setTimeout(() => {
-                    body.style.backgroundColor =`${array[4]}`;
-                    setTimeout(() => {
-                        body.style.backgroundColor =`${array[5]}`;
-                    }, 1000); 
-                }, 1000); 
-            }, 1000); 
-        }, 1000); 
-    }, 1000);       
+            body.style.backgroundColor = color;
+            resolve(`Changé en ${color}`);
+        }, 1000);
+    });
 }
 
-changeBGColor(colorArray);
+// Boucle de promesses pour chaque couleur
+changeBGColor(colorArray[0])
+    .then(() => changeBGColor(colorArray[1]))
+    .then(() => changeBGColor(colorArray[2]))
+    .then(() => changeBGColor(colorArray[3]))
+    .then(() => changeBGColor(colorArray[4]))
+    .then(() => changeBGColor(colorArray[5]))
+    .then(() => changeBGColor(colorArray[6]))
+    .catch((error) => console.error(error));
