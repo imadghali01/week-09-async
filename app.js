de1ca15682783066707555dea4fcb8d5a1cfba49
imadghali01 <imadghali01@gmail.com>
2024-10-29 16:14:40 +0100

    async-final

--- app.js
+++ app.js
@@ -1,59 +1,86 @@
-/*
-    Create a function generateBG that takes two parameters (color, delay)
-    This function returns a promise that turns the background color of the body color after delay time. Then it resolves.
-    Now you create an async function that calls and wait generateBG for each color given in the exercise :
-        blue
-        yellow
-        red
-        cyan
-        violet
-        green
-        purple
+/*OOOaaaaakaayyyy !
 
-Done ? Look at this code ! Simple and legible ! :)
+That was a lot of information on some basic concepts.
 
-Now, let's give it a litte bit of imprevisibility :
+We will now do an exercise to practice API calls and DOM manipulation.
 
-    In the function generateBG, create a random number between 0 and 0.999
-    If the random number is bigger than 0.5, reject the promise.
-    Run your program.
-    You'll see that at some point you'll get a native browser error (Uncaught (in promise))
-    We want to handle this error and not have the default error that breaks our code.
-    Use try/catch to console.log oh no! an error if there is a problem
+The mission
 
-(remember, you have a 50% chance of getting an error, so play with the condition in generateBG
-to have less or more chances )*/
+Using this api, create a little program as follow :
 
-const body = document.querySelector('body');
-const colorArray = ["yellow", "red", "cyan", "violet", "green", "purple"];
+    There is a text input where you should be able to type something related to food, a meal, etc.
 
-body.style.height = '100vh';
-const randomDelay = () => Math.random();
+    When you press ENTER, you should fetch all meals related to the search field.
 
-// Fonction qui retourne une promesse pour changer la couleur après "delay" seconde
-function generateBG(color, delay) {
-    return new Promise((resolve, reject) => {
-       setTimeout(()=>{
-            if(delay > 0.5){
-                    body.style.backgroundColor = color;
-                    resolve(`Changé en ${color}`);
-            }
-            else{
-                reject('oh no! an error')
-            }
-        }, (delay*2000))
+    You should then display in a grid the image of each meal and its title.
+
+    A little text should also say : these are the results for "chicken", for example
+
+    When you click on a meal, you should open a modal with the following meal information :
+        Image cover
+        Title
+        List of ingredients and how much of them you will need
+        A description on how to cook it.
+
+    You should be able to close the modal and search for another meal as many times you want.
+
+    Make it pretty ! Think of containers, menu, harmonic colors, etc.
+ */
+
+// ACCES
+
+const container = document.querySelector('.container');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const grid = document.querySelector('.grid');
+const modalImg = document.querySelector('.modalImg');
+const modalTitle = document.querySelector('.modalTitle');
+const modalList = document.querySelector('.modalList');
+const modalP = document.querySelector('.modalP');
+const query = input.value;
+let mealArray = {};
+
+
+//LISTENER AND CALL
+
+// on click button call the api server about the searched content and put all results in a contentArray (object array)
+
+const reponse = await fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+console.log(reponse);
+    const searchedmeal = async (search) =>{
+        reponse.forEach(meal => {
+            return new Promise ((resolve, reject) =>{
+                if(reponse){
+                    resolve('all good');
+                    mealArray[Object.key(reponse)] = reponse;
+                }
+                else{
+                    reject('oh an error happened');
+                }
+            })
     });
+    
 }
-// Boucle de promesses pour chaque couleur
-async function changeColor(){
-    for (let i = 0; i < colorArray.length; i++) {
-        try {
-            const message = await generateBG(colorArray[i], randomDelay());
-            console.log(message);
-        }
-        catch(error){
-            console.log(error);
-        }
+button.addEventListener('click', ()=>{
+    try{
+        searchedmeal(query);
+        console.log(reponse);
     }
-}
-changeColor();
+    catch(error){
+        console.log(error);
+    }
+})
+
+// foreach object of contentarray use the objectkey as gridcontent and push it into a clickable div in the .grid section(style it a bit)
+
+
+//SORT AND PUSH CONTENT
+
+// sort contentArray objects values into contentImg, contentGoodsList, contentDescription use the objectkey as contentTitle
+
+// push all in the apropriate mark
+
+
+
+
+
